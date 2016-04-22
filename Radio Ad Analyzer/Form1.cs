b0c94defcfd0462abde4d60f55994045eac7bccc
Salman Shafique <salmanshafique18@hotmail.com>
2016-04-23 00:09:26 +0500

    Implementation phase 1.

--- Radio Ad Analyzer/Form1.cs
+++ Radio Ad Analyzer/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
+using AXVLC;
+using AxAXVLC;
+
 
 namespace Radio_Ad_Analyzer
 {
@@ -15,24 +19,45 @@ namespace Radio_Ad_Analyzer
         public Form1()
         {
             InitializeComponent();
-           
-        }
-        public Color debugMode { get; set; }
-       
-        private void button8_Click(object sender, EventArgs e)
-        {
-        Button b = ((Button)sender);
-        b.Font = new Font(b.Font, FontStyle.Bold);
-    }
-        private void label44_Click(object sender, EventArgs e)
-        {
-         
         }
 
-        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        private void addAudioFilesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openfiledialog = new OpenFileDialog();
+
+            openfiledialog.Title = "Add Audio Files";
+            openfiledialog.Filter = ("MP3 Files(.mp3)|*.mp3|All Files (*.*)|*.*");
+            openfiledialog.CheckFileExists = true;
+
+            if (openfiledialog.ShowDialog() == DialogResult.OK)
+            {
+                var uri = new Uri(@openfiledialog.FileName);
+                var convertedURI = uri.AbsoluteUri;
+                axVLCPlugin21.playlist.add(convertedURI);
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            axVLCPlugin21.playlist.play();
+
+        }
+
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            axVLCPlugin21.playlist.pause();
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            axVLCPlugin21.playlist.stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-       
     }
 }
